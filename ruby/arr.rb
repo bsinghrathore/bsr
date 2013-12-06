11e0289bc421da6dde09dfb913899b0387855430
brijender singh rathore <bsrr38@gmail.com>
2013-12-07 02:52:06 +0530

    matrix

--- ruby/arr.rb
+++ ruby/arr.rb
@@ -1,25 +1,27 @@
 def arr(c)
-s=[3],[3]
-for i in 0..3
-  for j in 0..3
-s[j][i]=c[i][j]*3
+s=Array.new(3)
+for i in 0..2
+ s[i]=Array.new(3)
+  for j in 0..2
+ s[i][j]=c[i][j]*3
 end
  end
 puts"array after operation\n"
-for k in 0..3
-  for l in 0..3
+for k in 0..2
+  for l in 0..2
 puts "#{s[k][l]}"
 end 
  end
   end
 
-  a=[3],[3]
+  a=Array.new(3)
   
  
 puts"enter a  array\n"
-for row in 0..3
-  for col in 0..3
-a[row][col]= gets;
+for row in 0..2
+  a[row] = Array.new(3)	
+  for col in 0..2
+a[row][col]= gets.chomp.to_i;
 end
  end
  arr(a)
