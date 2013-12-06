c3596db1a590e97c5b74859073c1572572161ccc
brijender singh rathore <bsrr38@gmail.com>
2013-12-07 03:09:10 +0530

    matrix

--- ruby/pro.rb
+++ ruby/pro.rb
@@ -1,36 +1,40 @@
 def mul(c,d)
-m=[3],[3]
-for i in 0..3
-  for j in 0..3
+m= Array.new(3)
+for i in 0..2
+m[i] = Array.new(3)
+  for j in 0..2
    sum=0
-   for k in 0..3
-sum=sum+c[i][k]+d[k][j]
-end
+   for k in 0..2
+   sum=sum+c[i][k]+d[k][j]
+   end
 m[i][j]=sum
- end
-  end
+    end
+     end
+
 puts"array after multiplication"
-for k in 0..3
-  for l in 0..3
+for k in 0..2
+  for l in 0..2
 puts "#{m[k][l]}"
 end 
  end
   end
 
-  a=[3],[3]
-   b=[3],[3]
+  a=Array.new(3)
+  b=Array.new(3)
  
 puts"enter a  array\n"
-for row in 0..3
-  for col in 0..3
-a[row][col]= gets;
-end
- end
-
+for i in 0..2
+  a[i] = Array.new(3)
+  for j in 0..2
+   a[i][j] = gets.chomp.to_i;
+       end 
+         end
 puts"enter a  array\n"
-for row in 0..3
-  for col in 0..3
-b[row][col] = gets;
-end
- end
+for i in 0..2
+b[i] = Array.new(3)
+ for j in 0..2
+ b[i][j] = gets.chomp.to_i;
+  end
+    end
+ 
  mul(a,b)
