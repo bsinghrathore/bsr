be6852d2dfc20611d11a93e77e3ad2a3f4792894
brijender singh rathore <bsrr38@gmail.com>
2013-12-13 18:05:37 +0530

    passing struture

---
+++ new/structure.cpp
@@ -0,0 +1,33 @@
+
+struct Distance //English distance
+{
+int feet;
+float inches;
+};
+////////////////////////////////////////////////////////////////
+void engldisp( Distance ); //declaration
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+Distance d1, d2; //define two lengths
+//get length d1 from user
+cout << "Enter feet:"; cin >> d1.feet;
+cout << "Enter inches: "; cin >> d1.inches;
+//get length d2 from user
+cout << "\nEnter feet: "; cin >> d2.feet;
+cout << "Enter inches: "; cin >> d2.inches;
+cout << "\nd1 = ";
+engldisp(d1); //display length 1
+cout << "\nd2 = ";
+engldisp(d2); //display length 2
+cout << endl;
+getch();
+}
+//--------------------------------------------------------------
+// engldisp()
+// display structure of type Distance in feet and inches
+void engldisp( Distance dd ) //parameter dd of type Distance
+{
+cout << dd.feet << "\’-" << dd.inches << "\"";
+}
