293d24a3ef09427fcbf526463188173b1503ad70
brijender singh rathore <bsrr38@gmail.com>
2013-12-16 18:19:44 +0530

    class as data type

---
+++ new/data.cpp
@@ -0,0 +1,31 @@
+#include <iostream.h>
+#include<conio.h>
+class Distance 
+{
+private:
+int feet;
+float inches;
+public:
+void setdist(int ft, float in) 
+{ feet = ft; inches = in; }
+void getdist() 
+{
+cout << "\nEnter feet: "; cin >> feet;
+cout << "Enter inches: "; cin >> inches;
+}
+void showdist() 
+{ cout << feet << "\’-" << inches; 
+}
+};
+
+int main()
+{
+Distance dist1, dist2; 
+dist1.setdist(11, 6.25); 
+dist2.getdist(); 
+
+cout << "\ndist1 = "; dist1.showdist();
+cout << "\ndist2 = "; dist2.showdist();
+cout << endl;
+getch();
+}
