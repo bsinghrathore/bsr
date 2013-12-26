1e002a0d289c3ae89a6edd267c61a0c68e5a1474
brijender singh rathore <bsrr38@gmail.com>
2013-12-26 20:19:51 +0530

    prefix and postfix

---
+++ new/postfix.cpp
@@ -0,0 +1,37 @@
+#include <iostream.h>
+#include<conio.h>
+class Counter
+{
+private:
+unsigned int count; 
+public:
+Counter() : count(0) 
+{ }
+Counter(int c) : count(c) 
+{ }
+unsigned int get_count() const 
+{ return count; }
+Counter operator ++ () 
+{ 
+return Counter(++count); 
+} 
+Counter operator ++ (int) 
+{ 
+return Counter(count++); 
+} 
+};
+
+int main()
+{
+Counter c1, c2; 
+cout << "\nc1=" << c1.get_count(); 
+cout << "\nc2=" << c2.get_count();
+++c1; 
+c2 = ++c1; 
+cout << "\nc1=" << c1.get_count(); 
+cout << "\nc2=" << c2.get_count();
+c2 = c1++; 
+cout << "\nc1=" << c1.get_count(); 
+cout << "\nc2=" << c2.get_count() << endl;
+getch();
+}
