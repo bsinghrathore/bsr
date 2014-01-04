f7ef0884c94a79eb6cfc1ff92ef07c0cd3facbac
brijender singh rathore <bsrr38@gmail.com>
2014-01-04 19:41:28 +0530

    passing array through pointer

---
+++ pointer/pass.cpp
@@ -0,0 +1,19 @@
+#include <iostream.h>
+#include<conio.h>
+const int MAX = 5; 
+int main()
+{
+void centimize(double*); 
+double varray[MAX] = { 10.0, 43.1, 95.9, 59.7, 87.3 };
+centimize(varray); 
+for(int j=0; j<MAX; j++) 
+cout << "varray[" << j << "]="
+<< varray[j] << " centimeters" << endl;
+getch();
+}
+
+void centimize(double* ptrd)
+{
+for(int j=0; j<MAX; j++)
+*ptrd++ *= 2.54; 
+}
