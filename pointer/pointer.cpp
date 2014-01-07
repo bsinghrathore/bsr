39979b283500b15d15f01add74e91398a14157d8
brijender singh rathore <bsrr38@gmail.com>
2014-01-07 21:27:14 +0530

    array of pointer to object

---
+++ pointer/pointer.cpp
@@ -0,0 +1,40 @@
+#include<iostream.h>
+#include<conio.h>
+class person 
+{
+protected:
+char name[40]; 
+public:
+void setName() 
+{
+cout << "Enter name: ";
+cin >> name;
+}
+void printName() 
+{
+cout << "\n Name is: " << name;
+}
+};
+
+int main()
+{
+person* persPtr[100]; 
+int n = 0; 
+char choice;
+do 
+{
+persPtr[n] = new person; 
+persPtr[n]->setName(); 
+n++; 
+cout << "Enter another (y/n)? "; 
+cin >> choice; 
+}
+while( choice=='y' ); 
+for(int j=0; j<n; j++) 
+{ 
+cout << "\nPerson number " << j+1;
+persPtr[j]->printName();
+}
+cout << endl;
+getch();
+} 
