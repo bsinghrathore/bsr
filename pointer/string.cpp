c8a1f134f6382cfd33ecbbfb483544b12a86e50f
brijender singh rathore <bsrr38@gmail.com>
2014-01-07 18:46:30 +0530

    pointer to string constant

---
+++ pointer/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include<conio.h>
+int main()
+{
+char str1[] = "Defined as an array";
+char* str2 = "Defined as a pointer";
+cout << str1 << endl; 
+cout << str2 << endl;
+
+str2++; 
+cout << str2 << endl; 
+getch();
+}
