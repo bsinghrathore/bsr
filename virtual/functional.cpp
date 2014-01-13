745444f116026693fd3ceed299d3fbd2df3e48be
brijender singh rathore <bsrr38@gmail.com>
2014-01-13 19:43:14 +0530

    friends for Functional Notation

---
+++ virtual/functional.cpp
@@ -0,0 +1,33 @@
+#include <iostream.h>
+#include<conio.h>
+class Distance 
+{
+private:
+int feet;
+float inches;
+public: 
+Distance() : feet(0), inches(0.0)
+{ } 
+Distance(int ft, float in) : feet(ft), inches(in)
+{ }
+void showdist() 
+{ cout << feet << "\’-" << inches; }
+float square(); 
+};
+
+float Distance::square() 
+{ 
+float fltfeet = feet + inches/12; 
+float feetsqrd = fltfeet * fltfeet; 
+return feetsqrd; 
+}
+
+int main()
+{
+Distance dist(3, 6.0); 
+float sqft;
+sqft = dist.square(); 
+cout << "\nDistance = "; dist.showdist();
+cout << "\nSquare = " << sqft << " square feet\n";
+getch();
+}
